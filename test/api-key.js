bb3de7a9444f55e77c8f8ee8985c0a6ca289dd0b
Joshua <xeon826@gmail.com>
2024-06-27 17:44:34 -0500

    update

--- test/api-key.js
+++ test/api-key.js
@@ -1,4 +1,4 @@
 // Set your Google API key so that it can be used for the tests
 export default process.env.GOOGLE_API_TEST_KEY ||
   process.env.GOOGLE_API_KEY ||
-  'Replace with your Google API key'
+  'AIzaSyDen7KPKgQt24EOnSS45lvlmKz2tiIKw5E'
